50f19a45f1ecc566d980d5490955b7a9f02333e7
gyuhyunjang <jgh950829@gmail.com>
2019-10-09 18:48:35 +0900

    파일 복사
    drill file copy

--- Drill-05/move_character_with_mouse.py
+++ Drill-05/move_character_with_mouse.py
@@ -4,26 +4,25 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
 def handle_events():
-    global running, x, y, prev_x, prev_y, mouse_x, mouse_y
+    global running
+    global x,y
+    global mouse_x,mouse_y
+    global save_x,save_y
+    global move_ani
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
+            running =False
         elif event.type == SDL_MOUSEMOTION:
-            mouse_x,mouse_y = event.x, KPU_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-            prev_x,prev_y = x,y
-            x,y = event.x - 20,KPU_HEIGHT - 1 - event.y + 20
+            mouse_x, mouse_y = event.x+20, KPU_HEIGHT - 1 - event.y-10
+        elif event.type ==SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            save_x,save_y = x,y
+            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            move_ani =300
 
-    pass
+        elif event.type ==SDL_KEYDOWN and event.key ==SDLK_ESCAPE:
+            running =False
 
-def line(x1,y1,x2,y2):
-    for i in range(0, 100 + 1, 2):
-        t = i / 100
-        x = (1-t)*x1+t*x2
-        y = (1-t)*y1+t*y2
 
 
 open_canvas(KPU_WIDTH,KPU_HEIGHT)
@@ -33,19 +32,40 @@ Mouse = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-prev_x, prev_y, mouse_x, mouse_y = 0,0,0,0
+mouse_x, mouse_y =0,0
 frame = 0
+move_ani=300
+save_x,save_y=KPU_WIDTH // 2, KPU_HEIGHT // 2
 hide_cursor()
+
+def run():
+    global  x,y
+    global save_x,save_y
+    global running
+    frame=0
+    count = 0
+    while count<10:
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        Mouse.draw(mouse_x, mouse_y)
+        divine_x = (-x + save_x) / 10
+        divine_y = (-y + save_y) / 10
+        x = x + divine_x
+        y = y + divine_y
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        move_ani =300
+        count+=1
+        if(count == 10):
+            count=0
+            running=False
+
 
 
 while running:
-    clear_canvas()
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    Mouse.draw(mouse_x, mouse_y)
-    update_canvas()
-    frame = (frame + 1) % 8
 
+    run()
     handle_events()
 
 close_canvas()
