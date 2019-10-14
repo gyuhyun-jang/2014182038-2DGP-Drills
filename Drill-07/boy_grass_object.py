d97a3424c6cc608456e019ca5553ec6bb7acd950
gyuhyunjang <jgh950829@gmail.com>
2019-10-14 18:48:27 +0900

    sucess
    2014182038 장규현 2DGP Drill 07
    https://youtu.be/wtl3oqc2QRI
    커밋횟수 : 2회

--- Drill-07/boy_grass_object.py
+++ Drill-07/boy_grass_object.py
@@ -1,6 +1,46 @@
 from pico2d import *
+import random
 
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400,30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 800), 599
+        self.image_small = load_image('ball21x21.png')
+        self.image_big = load_image('ball41x41.png')
+        self.speed = random.randint(1,10)
+        self.ball_size = random.randint(0, 1)
+
+    def update(self):
+        if self.y <= 65:
+            self.y = 65
+        else:
+            self.y -= self.speed
+
+    def draw(self):
+        if self.ball_size == 0:
+            self.image_small.draw(self.x, self.y)
+        else:
+            self.image_big.draw(self.x, self.y)
 
 def handle_events():
     global running
@@ -12,7 +52,28 @@ def handle_events():
             running = False
 
 # initialization code
+open_canvas()
+boy = Boy()
+team = [Boy() for i in range(11)]
+balls = [Ball() for i in range(20)]
+grass = Grass()
+running = True
 
 # game main loop code
+while running:
+    handle_events()
+    for boy in team:
+        boy.update()
+    for ball in balls:
+        ball.update()
+    clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
+    for ball in balls:
+        ball.draw()
+    update_canvas()
+    delay(0.05)
 
 # finalization code
+close_canvas()
