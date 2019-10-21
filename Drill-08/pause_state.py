35372ead14333781b624733b308df1315b6ef214
gyuhyun-jang <55976879+gyuhyun-jang@users.noreply.github.com>
2019-10-21 20:14:12 +0900

    Drill-08-02 sucess
    2014182038 장규현 Drill-08-02
    commit : 2
    https://youtu.be/KuFV9MOOcdk

--- Drill-08/pause_state.py
+++ Drill-08/pause_state.py
@@ -5,9 +5,25 @@ from pico2d import*
 name = "PauseState"
 image = None
 
+class Pause:
+    def __init__(self):
+        self.image = load_image('pause.png')
+        self.flicker = 0
+
+    def update(self):
+        self.flicker = (self.flicker + 1) % 2
+        delay(0.1)
+
+    def draw(self):
+        if self.flicker == 0:
+            self.image.clip_draw(250,250,400,400,400,300)
+
+
+
 def enter():
     global image
     image = load_image('pause.png')
+    image = Pause()
 
 def exit():
     pass
@@ -20,10 +36,13 @@ def handle_events():
 
 def draw():
     clear_canvas()
-    image.draw(400,300)
+    image.draw()
+    main_state.boy.draw()
+    main_state.grass.draw()
     update_canvas()
 
 def update():
+    image.update()
     pass
 
 def pause():
